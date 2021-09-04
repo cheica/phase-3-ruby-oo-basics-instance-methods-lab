21e8cca5066e780ece8f7f179ea9e4e42c76ca23
Chelsea <chelsea.icaza10@hotmail.com>
2021-09-04 17:47:24 -0400

    commit tree d19fea4f748ca4554bec320d93d5fce3db8e794b

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+
+    def bark 
+        puts "Woof!"
+    end
+
+    def sit 
+    puts "The Dog is sitting"
+    end
+
+
+end
+
+nino = Dog.new
+nino.bark
+nino.sit
+
+snoopy = Dog.new 
+snoopy.bark
